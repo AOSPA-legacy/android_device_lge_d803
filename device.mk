69bd579f774951bc8a88fdcc9066e50a815441ff
houst0nn <houstonn@gmail.com>
2013-11-22 11:09:53 +0000

    d803: move gsm permissions over to common
    Signed-off-by: houstonn <houstonn@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,3 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PROPERTY_OVERRIDES += \
         telephony.lteOnCdmaDevice=1 \
         ro.telephony.default_network=10
-
-PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
